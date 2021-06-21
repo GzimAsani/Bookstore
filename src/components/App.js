e0cc1602a3ed85bef91d82cb3086fc03522c96c7
Gzim <gzimasanni@gmail.com>
2021-06-21 10:59:42 +0200

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import Navbar from './Navbar';
 
 const App = () => (
   <div>
-    <BooksForm />
+    <Navbar />
     <BooksList />
+    <BooksForm />
   </div>
 );
 
